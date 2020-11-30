50b5f0698ccaa62dac830005d271eb7478e10180
Shay O'Connell <shay.oconnell7@gmail.com>
2020-11-30 13:29:12 -0500

    imported scrape function

---
+++ mission_to_mars/app.py
@@ -0,0 +1,40 @@
+# 1. import Flask
+from flask import Flask, render_template
+from scrape_mars import scrape
+import pymongo
+
+# 2. Create an app, being sure to pass __name__
+app = Flask(__name__)
+
+
+# setup mongo connection
+conn = "mongodb://localhost:27017"
+client = pymongo.MongoClient(conn)
+
+# connect to mongo db and collection
+
+db = client.mars_info
+info_dict = db.info_dict
+
+#homepage
+@app.route("/")
+def index():
+    # write a statement that finds all the items in the db and sets it to a variable
+    info_dict.insert_one(scrape)
+    
+    print(info_dict)
+
+    # render an index.html template and pass it the data you retrieved from the database
+    return render_template("index.html", inventory=inventory)
+
+#scrape_mars.scrape
+
+# 4. Define what to do when a user hits the /scrape route
+# @app.route("/scrape")
+# def about():
+#     print("Server received request for 'About' page...")
+#     return "Welcome to my 'About' page!"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- mission_to_mars/scrape_mars.py
+++ mission_to_mars/scrape_mars.py
@@ -18,176 +18,125 @@ import requests
 from splinter import Browser
 from bs4 import BeautifulSoup as bs
 
+app = Flask(__name__)
 
-executable_path = {"executable_path": "chromedriver"}
-browser = Browser("chrome", **executable_path, headless=False)
+@app.route("/scrape")
+def scrape():
 
+    executable_path = {"executable_path": "chromedriver"}
+    browser = Browser("chrome", **executable_path, headless=False)
 
+    # +
+    # browser = init_browser()
 
+    url = "https://mars.nasa.gov/news/"
+    browser.visit(url)
 
+    html = browser.html
+    soup = bs(html, "html.parser")
 
-# +
-# browser = init_browser()
+    # -
 
-url = "https://mars.nasa.gov/news/"
-browser.visit(url)
+    # ## NASA Mars News
 
-html = browser.html
-soup = bs(html, "html.parser")
+    sidebar = soup.find('ul', class_='item_list')
+    print(sidebar)
 
-# -
+    articles = sidebar.find_all('li')
 
-# ## NASA Mars News
+    for article in articles [0]:
+        teaser = article.select_one('div.article_teaser_body')
+        title = article.select_one('div.content_title a')
+        
 
-sidebar = soup.find('ul', class_='item_list')
-print(sidebar)
+    teaser.text
 
-articles = sidebar.find_all('li')
+    title.text
 
-for article in articles [0]:
-    teaser = article.select_one('div.article_teaser_body')
-    title = article.select_one('div.content_title a')
-    
+    print(f"The latest headline: {title.text}.\nThe summary: {teaser.text}")
 
-teaser.text
+    # ## JPL Mars Space Images - Featured Image
+    #
 
-title.text
+    # +
+    url = "https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars"
+    browser.visit(url)
+    short_url = "https://www.jpl.nasa.gov"
 
-print(f"The latest headline: {title.text}.\nThe summary: {teaser.text}")
+    html = browser.html
+    soup = bs(html, "html.parser")
+    # -
 
-# ## JPL Mars Space Images - Featured Image
-#
+    med_extension = soup.find("div", class_="carousel_items").a["data-fancybox-href"]
+    med_image_url = (short_url+med_extension)
+    print(f"url is {med_image_url}")
 
-# +
-url = "https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars"
-browser.visit(url)
-short_url = "https://www.jpl.nasa.gov"
+    extension = soup.find("li", class_="slide").a["data-fancybox-href"]
+    featured_image_url = (short_url+extension)
+    print(f"url is {featured_image_url}")
 
-html = browser.html
-soup = bs(html, "html.parser")
-# -
+    # ## Mars Facts
 
-med_extension = soup.find("div", class_="carousel_items").a["data-fancybox-href"]
-med_image_url = (short_url+med_extension)
-print(f"url is {med_image_url}")
+    # +
+    url = "https://space-facts.com/mars/"
+    browser.visit(url)
 
-extension = soup.find("li", class_="slide").a["data-fancybox-href"]
-featured_image_url = (short_url+extension)
-print(f"url is {featured_image_url}")
+    html = browser.html
+    soup = bs(html, "html.parser")
+    # -
 
-# ## Mars Facts
+    tables = pd.read_html(url)
+    tables
 
-# +
-url = "https://space-facts.com/mars/"
-browser.visit(url)
+    mars_info = pd.DataFrame(tables[0])
+    mars_info.columns = ['Mars', 'Data']
+    mars_info.set_index('Mars', inplace=True)
 
-html = browser.html
-soup = bs(html, "html.parser")
-# -
+    mars_info
 
-tables = pd.read_html(url)
-tables
+    mars_html_table = mars_info.to_html()
+    mars_html_table
 
-mars_info = pd.DataFrame(tables[0])
-mars_info.columns = ['Mars', 'Data']
-mars_info.set_index('Mars', inplace=True)
+    mars_html_table.replace('\n', '')
 
-mars_info
+    # ## Mars Hemispheres
 
-mars_html_table = mars_info.to_html()
-mars_html_table
+    url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
+    browser.visit(url)
 
-mars_html_table.replace('\n', '')
+    # +
+    hemisphere_image_urls = []
 
-# ## Mars Hemispheres
+    # First, get a list of all of the hemispheres
+    links = browser.find_by_css("a.product-item h3")
 
-# +
-url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
-browser.visit(url)
+    # Next, loop through those links, click the link, find the sample anchor, return the href
+    for i in range(len(links)):
+        hemisphere = {}
+        
+        # We have to find the elements on each loop to avoid a stale element exception
+        browser.find_by_css("a.product-item h3")[i].click()
+        
+        # Get Hemisphere title
+        hemisphere['title'] = browser.find_by_css("h2.title").text
+        
+        # Next, we find the Sample image anchor tag and extract the href
+        sample_elem = browser.links.find_by_text('Sample').first
+        hemisphere['img_url'] = sample_elem['href']
+        
+        # Append hemisphere object to list
+        hemisphere_image_urls.append(hemisphere)
+        
+        # Finally, we navigate backwards
+        browser.back()
+    # -
 
-html = browser.html
-soup = bs(html, "html.parser")
+    hemisphere_image_urls
 
-# +
-# Visit the mars nasa news site
-url = 'https://mars.nasa.gov/news/'
-browser.visit(url)
 
-# Optional delay for loading the page
-browser.is_element_present_by_css("ul.item_list li.slide", wait_time=1)
 
-# +
 
-sidebar = soup.find(class_='collapsible_results')
 
-categories = sidebar.find_all('item')
 
-category_list = []
-url_list = []
-book_url_list = []
-
-for category in categories:
-    title = category.text.strip()
-    category_list.append(title)
-    book_url = category.find('a')['href']
-    url_list.append(book_url)
-
-book_url_list = ['http://books.toscrape.com/' + url for url in url_list]
-
-titles_and_urls = zip(category_list, book_url_list)
-
-try:
-    for title_url in titles_and_urls:
-        browser.click_link_by_partial_text('next')
-except ElementDoesNotExist:
-    print("Scraping Complete")
-# -
-
-browser.click_link_by_partial_text('Enhanced')
-
-links_found = browser.links.find_by_partial_text('Enhanced')
-
-links_found.text
-
-
-images_a = browser.find_by_css("a.product-item h3")
-
-img_dict = {"title" : "", "img_url" : ""}
-
-hemisphere_image_urls = []
-
-for image in images_a:
-    browser.links.find_by_partial_text('Enhanced').click
-    img_dict.update(img_url = image)
-    img_dict["title"] = image.text
-    hemisphere_image_urls.append(img_dict)
-    print(image)
-
-img_dict
-
-hemisphere_image_urls
-
-print(images_a)
-
-images_a.text
-
-urls.text
-
-browser.find_by_css('item').first.value
-
-
-
-
-
-
-
-
-
-hemisphere_image_urls = [
-    {"title": "Valles Marineris Hemisphere", "img_url": "..."},
-    {"title": "Cerberus Hemisphere", "img_url": "..."},
-    {"title": "Schiaparelli Hemisphere", "img_url": "..."},
-    {"title": "Syrtis Major Hemisphere", "img_url": "..."},
-]
 
 
